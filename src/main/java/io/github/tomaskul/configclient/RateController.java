ce7a7aba5fa664a5783a811a7b94183bde9f2dbd
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-04-20 17:05:24 +0100

    Add Spring Boot Cloud-Client application, connect it to cloud config server, add RateController for proof-of-concept show of configurable value travelling from git repo -> config server -> config client -> UI.

---
+++ src/main/java/io/github/tomaskul/configclient/RateController.java
@@ -0,0 +1,26 @@
+package io.github.tomaskul.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RateController {
+    @Value("${rate}")
+    String rate;
+    @Value("${lanecount}")
+    String lanecount;
+    @Value("${tollstart}")
+    String tollstart;
+
+    @RequestMapping("/rate")
+    public String getRate(Model m) {
+        m.addAttribute("rateamount", rate);
+        m.addAttribute("lanes", lanecount);
+        m.addAttribute("tollstart", tollstart);
+
+        return "rateview";
+    }
+
+}
