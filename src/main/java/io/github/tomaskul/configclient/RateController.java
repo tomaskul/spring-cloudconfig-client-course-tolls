26bb43d1db382edfe904038cb645d0a08a83f00a
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-04-21 11:21:08 +0100

    Add client-side config refresh. Refresh pushed via POST /actuator/refresh against client.

--- src/main/java/io/github/tomaskul/configclient/RateController.java
+++ src/main/java/io/github/tomaskul/configclient/RateController.java
@@ -1,10 +1,12 @@
 package io.github.tomaskul.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RefreshScope
 @Controller
 public class RateController {
     @Value("${rate}")
